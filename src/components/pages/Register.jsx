dfff42c48d013f82eeca3c9cd54a16fadbdd5fb0
Khaled Bin Monowar Hossain <khaledbinmonowarhossain@gmail.com>
2025-01-15 01:00:36 +0600

    register update

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -76,20 +76,7 @@ function Register() {
           </button>
         </div>
       </form>
-      <div className="divider -my-3">OR</div>
-      <div
-        onClick={signInWithGoogle}
-        className="text-lg px-4 py-2 bg-white/10 rounded-full"
-      >
-        <button className="flex items-center gap-2">
-          <p>Continue with</p>
-          <img
-            className="w-16 mt-1"
-            src="https://pngimg.com/d/google_PNG19644.png"
-            alt="google icon"
-          />
-        </button>
-      </div>
+      
       <p className="text-sm">
         Already a user?{' '}
         <Link to="/auth/login" className="underline">
