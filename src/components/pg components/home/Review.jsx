37a0762a78bf48ddde6e9c29dd883ed0e62b4f4f
Khaled Bin Monowar Hossain <khaledbinmonowarhossain@gmail.com>
2025-05-02 17:37:43 +0600

    review card style change

--- src/components/pg components/home/Review.jsx
+++ src/components/pg components/home/Review.jsx
@@ -2,21 +2,21 @@ function Review() {
   const reviews = [
     {
       name: 'David Carter',
-      img: 'https://images.unsplash.com/photo-1597612041762-93a90e22af06',
+      img: 'https://img.freepik.com/free-photo/positive-young-man_1385-2287.jpg',
       profession: 'Tech Enthusiast',
       review:
         'Newtova is an amazing platform for discovering the latest tech products! The upvoting system helps highlight the best innovations, and the reviews give real user insights. Highly recommended for tech lovers!',
     },
     {
       name: 'Daniel Roberts',
-      img: 'https://images.unsplash.com/photo-1556474835-b0f3ac40d4d1',
+      img: 'https://st3.depositphotos.com/1715570/14089/i/450/depositphotos_140892126-stock-photo-laughing-young-african-guy.jpg',
       profession: 'Startup Founder',
       review:
         'As a founder, getting my product in front of the right audience is crucial. Newtova’s submission and upvoting system make it easier to gain visibility. The premium features are worth it for extra exposure!',
     },
     {
       name: 'Handy Wilson',
-      img: 'https://images.unsplash.com/photo-1521132293557-5b908a59d1e1',
+      img: 'https://img.freepik.com/free-photo/portrait-smiling-handsome-man-eyeglasses_171337-4853.jpg?t=st=1746185601~exp=1746189201~hmac=7cb1578e7ce3f3ebb28ed3c924d565ea5a9a69e893c23a94286860b03a9156ff&w=1380',
       profession: 'Software Developer',
       review:
         'I love how user-friendly and well-designed Newtova is! The authentication system is seamless, and the dashboard makes managing my submissions effortless. A must-visit for anyone in the tech space!',
@@ -30,7 +30,7 @@ function Review() {
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-between flex-grow">
         {reviews.map((review) => (
-          <div key={review.name} className="mid p-4 rounded-lg">
+          <div key={review.name} className="mid p-4 rounded-lg border border-[#4a484a] hover:border-[#fff]">
             <div className="flex items-center space-x-4">
               <img
                 src={review.img}
@@ -38,11 +38,11 @@ function Review() {
                 className="w-14 h-14 rounded-full object-cover"
               />
               <div>
-                <h3 className="text-lg font-semibold">{review.name}</h3>
-                <p className="text-sm">{review.profession}</p>
+                <h3 className="text-lg font-semibold text-left">{review.name}</h3>
+                <p className="text-sm opacity-70 text-left">{review.profession}</p>
               </div>
             </div>
-            <p className="mt-3">{review.review}</p>
+            <p className="mt-3 text-left">{review.review}</p>
           </div>
         ))}
       </div>
